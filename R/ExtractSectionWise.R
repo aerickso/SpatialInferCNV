5fd0354d08141a34fcca5832e0129ca683c607bc
Andrew Erickson <ericksonandrewm@gmail.com>
2022-03-15 11:41:57 +0200

    Updating ExtractSectionWise() documentation.

--- R/ExtractSectionWise.R
+++ R/ExtractSectionWise.R
@@ -1,7 +1,18 @@
-#' ExtractSectionWise
+#' Obtaining a thresholded dataframe as part of spatial visualization of spatial transcriptomics data.
 #'
 #' ExtractSectionWise()
-
+#' 
+#' @param SectionName A character string for section name.
+#' @param CNV_Genes_Organscale_Input A dataframe, mirroring the structure of infercnv::run output file 17_HMM_predHMMi6.hmm_mode-cells.pred_cnv_genes.dat
+#' @param AllBarcodes A dataframe of barcodes and annotations.
+#' @param Threshold A numerical value for sectionwise thresholding of the number of genes to pass: integer values from 0-100.
+#' 
+#' @return A dataframe of ST counts, that have passed QC and are selected.
+#' 
+#' @examples
+#' ExtractSectionWise("H2_1", CNV_Genes_Filtered, AllBarcodes, 0.45)
+ 
+ 
 ExtractSectionWise <- function(SectionName, CNV_Genes_Organscale_Input, AllBarcodes, Threshold) {
   output <- CNV_Genes_Organscale_Input %>%
     filter(section == paste0(SectionName)) %>%
