6777c9fb34f56b2ae09e9a4096a9f01ef66f8058
Andrew Erickson <ericksonandrewm@gmail.com>
2022-03-15 11:48:14 +0200

    Updating Output_PGA_Visualization_MatrixGreyNA() documentation

--- R/Output_PGA_Visualization_MatrixGreyNA.R
+++ R/Output_PGA_Visualization_MatrixGreyNA.R
@@ -1,6 +1,14 @@
-#' Output_PGA_Visualization_MatrixGreyNA
+#' Preparing a matrix for spatial visualization of number of genes with an inferred CNV, derived from spatial transriptomics data.
 #'
 #' Output_PGA_Visualization_MatrixGreyNA()
+#' 
+#' @param SectionName A character string for section name.
+#' @param InputCNVs An input dataframe created by the function ExtractSectionWise()
+#' @param BarcodesFile A single column dataframe comprised of a list of barcode coordinates in the form AxB, where A = the X coordinate, and B = the Y coordinate.
+#' 
+#' @return A dataframe for spatial visualization by Plot_PGA_Visualization_Matrix()
+#' @examples
+#' Output_PGA_Visualization_MatrixGreyNA("H2_1", H2_1_Sectionwise_CNVsGenes_Counted, L2_Barcodes)
 
 Output_PGA_Visualization_MatrixGreyNA <- function(SectionName, InputCNVs, BarcodesFile) {
   input <- InputCNVs %>%
