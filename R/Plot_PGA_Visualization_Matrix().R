e558b6a8c2c674558d16c6f111960c4fc46870d0
Andrew Erickson <ericksonandrewm@gmail.com>
2022-03-15 11:52:26 +0200

    Updating Plot_PGA_Visualization_Matrix() documentation

--- R/Plot_PGA_Visualization_Matrix().R
+++ R/Plot_PGA_Visualization_Matrix().R
@@ -1,6 +1,14 @@
 #' Plotting the spatial distribution of genes with an inferred copy number alteration from an underlying matrix
 #'
 #' Plot_PGA_Visualization_Matrix()
+#' 
+#' @param SectionName A character string for section name.
+#' @param InputMatrix An input matrix created by the function Output_PGA_Visualization_MatrixGreyNA()
+#' @param MaxValInput An upper threshold for plotting, derived from the maximum sectionwise value of the number of inferred genes with a CNV (from ExtractSectionWise()) 
+#' 
+#' @return An output spatial visualization of the number of genes with an inferred CNV from 1k array spatial transcriptomics data.
+#' @examples
+#' Plot_PGA_Visualization_Matrix("H2_1", PGA_Matrix, MaxVal)
 
 Plot_PGA_Visualization_Matrix <- function(SectionName, InputMatrix, MaxValInput) {
   ggplot(InputMatrix, aes(x = x, y = y)) +
