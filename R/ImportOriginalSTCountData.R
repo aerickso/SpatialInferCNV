de645678652c983f4032b9693f87224087aa58df
Andrew Erickson <ericksonandrewm@gmail.com>
2022-03-15 11:22:23 +0200

    Updating ImportOriginalStCountData.R documentation

--- R/ImportOriginalSTCountData.R
+++ R/ImportOriginalSTCountData.R
@@ -1,6 +1,12 @@
-#' Importing Histological Annotations
+#' Importing spatial transcriptomics, 1k array count data and append section names to the barcodes.
 #'
 #' ImportHistologicalAnnotations()
+#'
+#' @param SectionName A character string for section name.
+#' @param InputCountFile A file path to a .tsv file
+#' @return A dataframe of count data, having barcodes with appended section names
+#' @examples
+#' ImportOriginalSTCountData("H2_1", "./Patient 1/1k_arrays/H2_1/180903_L11_CN63_D1_H2.1_EB_stdata.tsv")
 
 ImportOriginalSTCountData <- function(SectionName, InputCountFile) {
   input <- as.data.frame(read.delim(InputCountFile, row.names = 1))
