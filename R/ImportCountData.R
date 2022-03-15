96f28ecf6171e2c695479c999f0ae3675b1a663e
Andrew Erickson <ericksonandrewm@gmail.com>
2022-03-15 13:16:55 +0200

    Updating ImportCountData() documentation

--- R/ImportCountData.R
+++ R/ImportCountData.R
@@ -1,6 +1,12 @@
-#' Importing Histological Annotations
+#' Importing Visium spatial transcriptomics count data from filtered_feature_bc_matrix.h5 file (output from SpaceRanger pipeline) and appending section name to barcodes
 #'
-#' ImportHistologicalAnnotations()
+#' ImportCountData()
+#' 
+#' @param SectionName A character string for section name.
+#' @param InputCountFile A file path to a filtered_feature_bc_matrix.h5 file (output from 10X Genomics SpaceRanger pipeline)
+#' @return A dataframe of barcodes with appended section names
+#' @examples
+#' ImportCountData("H2_1", "./filtered_feature_bc_matrix.h5")
 
 ImportCountData <- function(SectionName, InputCountFile) {
   input <- Read10X_h5(InputCountFile, use.names = FALSE)
