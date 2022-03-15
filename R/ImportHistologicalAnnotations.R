b43c45bde53ebd1b6ec11b3fe53fd5ba763da1d9
Andrew Erickson <ericksonandrewm@gmail.com>
2022-03-15 13:13:45 +0200

    Updating ImportHistologicalAnnotations documentation

--- R/ImportHistologicalAnnotations.R
+++ R/ImportHistologicalAnnotations.R
@@ -1,6 +1,12 @@
-#' Importing Histological Annotations
+#' Importing histological annotations of Visium barcodes and appending a section name to the barcodes.
 #'
 #' ImportHistologicalAnnotations()
+#' 
+#' @param SectionName A character string for section name.
+#' @param InputAnnotationFile A file path to a .tsv file
+#' @return A dataframe of barcodes with appended section names
+#' @examples
+#' ImportHistologicalAnnotations("H1_2", "./H1_2_Final_Consensus_Annotations.csv")
 
 ImportHistologicalAnnotations <- function(SectionName, InputAnnotationFile) {
   input <- read.csv(paste0(InputAnnotationFile))
