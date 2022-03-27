e23df3d66b1e148ea371f23bb9d086352af3c330
Andrew Erickson <ericksonandrewm@gmail.com>
2022-03-27 19:20:04 +0300

    Tweaking one function to be more robust

--- R/Output_PGA_Visualization_MatrixGreyNA.R
+++ R/Output_PGA_Visualization_MatrixGreyNA.R
@@ -11,14 +11,11 @@
 #' Output_PGA_Visualization_MatrixGreyNA("H2_1", H2_1_Sectionwise_CNVsGenes_Counted, L2_Barcodes)
 
 Output_PGA_Visualization_MatrixGreyNA <- function(SectionName, InputCNVs, BarcodesFile) {
-  input <- InputCNVs %>%
-    mutate(section = substr(Barcode, 1, 4)) %>%
-    filter(section == paste0(SectionName)) %>%
-    mutate(Barcode = substr(Barcode, 6, max(nchar(Barcode))))
+  input <- InputCNVs %>% extract(Barcode, c("Barcode", "XY"), "(.*)_([^_]+)") %>% select(-Barcode)
+  names(input)[1] <- "Barcode"
   PGA_Visualization_Matrix <- left_join(BarcodesFile, input)
-  PGA_Visualization_Matrix$Test <- sub('.*\\_', '', PGA_Visualization_Matrix$Barcode)
-  PGA_Visualization_Matrix$x <- as.numeric(sub('.*x', '', PGA_Visualization_Matrix$Test))
-  PGA_Visualization_Matrix$y <- as.numeric(sub('x.*', '', PGA_Visualization_Matrix$Test))
+  PGA_Visualization_Matrix$x <- as.numeric(sub('.*x', '', PGA_Visualization_Matrix$Barcode))
+  PGA_Visualization_Matrix$y <- as.numeric(sub('x.*', '', PGA_Visualization_Matrix$Barcode))
   PGA_Visualization_Matrix <- PGA_Visualization_Matrix %>%
     select(x, y, PercentageGenomeAltered) %>%
     arrange(x, y)
